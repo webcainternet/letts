23a54b0b0afdb516aede12dcde45a4ce08ac0da7
root <root@uspro575.startdedicated.com>
2014-08-27 15:33:05 +0000

    up producao

--- public/wp-config.php
+++ public/wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'letts');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'letts');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'jf3uUsjwd');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
