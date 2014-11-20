eea7b8f2bee4aa7fdbbd0d059ec97ed9cf099cda
fernandofmendes <fernando.mendes@webca.com.br>
2014-11-20 17:31:21 -0200

    Marcas

--- public/wp-content/themes/magazine/single-marca.php
+++ public/wp-content/themes/magazine/single-marca.php
@@ -24,40 +24,89 @@ global $themify; ?>
 
 	
 		
-	<img src="http://letts.com.br/wp-content/uploads/2014/07/nike-ronalod-orange-mercurial-vapor-c_1600x1200_83114.jpg" width="100%">
+	<div class="img_marcas" style="background: url('<?php print_custom_field('basicaimagem:to_image_src'); ?>') no-repeat;"></div>
 
-<div style="float: left;
-				margin: 15px;
-				margin-left: 25px;">
+	<div class="logo_marcas" style="background: url('<?php print_custom_field('logo:to_image_src'); ?>') no-repeat;"></div>
+
+	<div style="float: left; margin: 15px; margin-left: 25px; width: 420px;">
 		<h1 class="post-title entry-title" itemprop="name">
-			<a href="#" title="<?php the_title(); ?>">
 				<?php the_title(); ?>
-			</a>
 		</h1>
+		<?php print_custom_field('endereco:do_shortcode'); ?>
+		<p>Telefones: <?php print_custom_field('basicatelefones'); ?></p>
+	</div>
+	<div class="icones_sociais">
+		<?php if (get_custom_field('basicafacebook')) { ?>
+			<a href="<?php print_custom_field('basicafacebook'); ?>" target="_blank">
+				<img src="/wp-content/themes/magazine/images/facebook.png">
+			</a>
+		<?php } ?>
+
+		<?php if (get_custom_field('instagram')) { ?>
+			<a href="<?php print_custom_field('instagram'); ?>" target="_blank">
+				<img src="/wp-content/themes/magazine/images/instagram.png">
+			</a>
+		<?php } ?>
+
+		<?php if (get_custom_field('twitter')) { ?>
+			<a href="<?php print_custom_field('twitter'); ?>" target="_blank">
+				<img src="/wp-content/themes/magazine/images/twitter.png">
+			</a>
+		<?php } ?>
+
+		<?php if (get_custom_field('linkedin')) { ?>
+			<a href="<?php print_custom_field('linkedin'); ?>" target="_blank">
+				<img src="/wp-content/themes/magazine/images/linkedin.png">
+			</a>
+		<?php } ?>
+
+		<?php if (get_custom_field('basicaemail')) { ?>
+			<a href="<?php print_custom_field('basicaemail'); ?>" target="_blank">
+				<img src="/wp-content/themes/magazine/images/contacts.png">
+			</a>
+		<?php } ?>
+
+		<?php if (get_custom_field('blog')) { ?>
+			<a href="<?php print_custom_field('blog'); ?>" target="_blank">
+				<img src="/wp-content/themes/magazine/images/rss.png">
+			</a>
+		<?php } ?>								
+
+		<?php if (get_custom_field('site')) { ?>
+			<a href="<?php print_custom_field('site'); ?>" target="_blank">
+				<img src="/wp-content/themes/magazine/images/domain.png">
+			</a>
+		<?php } ?>
+
 	</div>
 
-<?php the_content(); ?>
+	<div class="description_marcas">
+		<?php the_content(); ?>
+	</div>
 
-<div>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet malesuada odio, sit amet tempor orci. Nullam at dolor eu tellus molestie porttitor. Phasellus porttitor augue sit amet magna lacinia convallis. Nullam malesuada felis sollicitudin nunc mollis, gravida accumsan enim commodo. Curabitur congue, augue non dictum bibendum, augue metus faucibus diam, sed tempus tortor diam vel leo. Donec volutpat pretium nibh, id accumsan diam congue at. Nullam urna lacus, imperdiet in augue sed, malesuada sodales lorem. Nam euismod magna eget risus adipiscing, imperdiet interdum neque pulvinar. Duis in posuere metus. Curabitur et tempor purus. Maecenas molestie, lorem suscipit tristique aliquam, turpis ante ultrices tortor, sit amet malesuada libero ligula nec lorem.
-</div>
-<br />
-<div>
-	Sed vel gravida sapien. Sed fermentum diam ligula, nec pulvinar est commodo eget. Nunc bibendum mi dapibus tellus porttitor, sit amet feugiat turpis volutpat. Maecenas nec arcu felis. Proin eleifend nisl eget euismod varius. Vestibulum in lectus sed ligula laoreet iaculis. Nunc eget nisl mollis, mollis risus at, elementum elit. Aenean congue accumsan enim, id vestibulum sapien mollis hendrerit. Aenean vestibulum diam a nunc cursus ullamcorper. Nam aliquet luctus erat non elementum. Etiam sagittis, nibh facilisis commodo rhoncus, ipsum metus ornare purus, a interdum tortor nulla ac sapien. Phasellus euismod eu ligula in lacinia. Nulla sit amet nulla rhoncus, bibendum dui porttitor, vestibulum eros. Aliquam erat volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-</div>
-<br />
+	<div class="formularios">
+		<div class="convidar_atleta">
+			<h1 class="post-title entry-title">Convide um Atleta</h1>
+			<form action="" method="post" id="formulario_convite">
+				<input type="text" id="nome" name="nome" placeholder="Seu Nome">
+				<input type="text" id="nome_atleta" name="nome_atleta" placeholder="Nome do Atleta">
+				<input type="text" id="email_atleta" name="email_atleta" placeholder="E-mail do Atleta">
+				<textarea id="mensagem_atleta" name="mensagem_atleta" placeholder="Mensagem ao Atleta"></textarea>
+				<input type="button" id="botao_convite" value="Convidar Atleta">
+			</form>
+		</div>
 
-<div style="text-align: center; width: 100%;">
-	<img src="http://letts.com.br/wp-content/uploads/2014/07/demo001.png">
-</div>
-<br />
-
-
-<div style="float: right; background-color: #ff8920; color: white; padding: 10px;" >
-	Enviar mensagem
-</div>
-<br />
-
+		<div class="mensagem_marca">
+			<h1 class="post-title entry-title">Envie mensagem para a marca</h1>
+			<form action="" method="post" id="formulario_mensagem">
+				<input type="text" id="nome_msg" name="nome_msg" placeholder="Seu Nome">
+				<input type="text" id="email_msg" name="email_msg" placeholder="Seu E-mail">
+				<input type="text" id="assunto" name="assunto" placeholder="Assunto">
+				<textarea id="mensagem" name="mensagem" placeholder="Mensagem para a Marca"></textarea>
+				<input type="button" id="botao_mensagem" value="Enviar Mensagem">
+			</form>
+		</div>		
+	</div>
 
 
 <?php endwhile; ?>
