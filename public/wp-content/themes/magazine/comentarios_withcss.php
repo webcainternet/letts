193370e25b157bd67bb97a100079fa3f0175168b
fernandofmendes <fernando.mendes@webca.com.br>
2015-12-04 06:30:33 -0200

    comentários em iframe

---
+++ public/wp-content/themes/magazine/comentarios_withcss.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+
+<head>
+	<link rel='stylesheet' id='theme-style-css'  href='http://letts.com.br/wp-content/themes/magazine/style.css?ver=1.3.0' type='text/css' media='all' />
+</head>
+
+<?php include "comentarios.php"; ?>
+
