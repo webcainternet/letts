5719b37317e6b496809748bbd066da6bdb24c65a
fernandofmendes <fernando.mendes@webca.com.br>
2015-12-04 06:31:48 -0200

    fix

--- public/wp-content/themes/magazine/comentarios_withcss.php
+++ public/wp-content/themes/magazine/comentarios_withcss.php
@@ -4,5 +4,6 @@
 	<link rel='stylesheet' id='theme-style-css'  href='http://letts.com.br/wp-content/themes/magazine/style.css?ver=1.3.0' type='text/css' media='all' />
 </head>
 
-<?php include "comentarios.php"; ?>
-
+<div style="width: 615px;">
+	<?php include "comentarios.php"; ?>
+</div>
