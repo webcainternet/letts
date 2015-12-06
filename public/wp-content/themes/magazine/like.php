78ae676bef379de2087271af8a2bea2cdbdba336
fernandofmendes <fernando.mendes@webca.com.br>
2015-12-06 19:03:32 -0200

    urlcss fix

--- public/wp-content/themes/magazine/like.php
+++ public/wp-content/themes/magazine/like.php
@@ -1,7 +1,7 @@
 <head>
 <meta charset="UTF-8">
 
-<link rel="stylesheet" id="style.css-css" href="/wp-content/plugins/google-language-translator/css/style.css?ver=4.1.1" type="text/css" media="all">
+<link rel='stylesheet' id='theme-style-css'  href='/wp-content/themes/magazine/style.css?ver=1.3.0' type='text/css' media='all' />
 <link rel="stylesheet" id="google-fonts-css" href="//fonts.googleapis.com/css?family=Oswald%7COpen+Sans%7COpen+Sans%3A300&amp;subset=latin%2Clatin-ext&amp;ver=4.1.1" type="text/css" media="all">
 </head>
 
